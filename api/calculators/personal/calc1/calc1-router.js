7e9f46cb8a226335b141673efcbb29e49f025688
MWeberLambdaweb19 <mack.webb37@gmail.com>
2019-07-31 10:29:12 -0700

    Updated readme to new standards and endpoints, added further functionality to server

--- api/calculators/personal/calc1/calc1-router.js
+++ api/calculators/personal/calc1/calc1-router.js
@@ -14,18 +14,35 @@ router.get('/', (req, res) => {
 
 router.get('/:id', (req, res) => {
     const {id} = req.params
-    calc1.findById(id)
+    calc1.findByUser(id)
     .then(calc => {
         if (calc) {
             res.status(200).json(calc)
         } else {
-            res.status(404).json({message: "That user's personal running total calculator does not exist!"})
+            res.status(404).json({message: "Could not retrieve calculators by user ID"})
+        }
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json(err)
+    })
+});
+
+router.get('/:userId/:id', (req, res) => {
+    const {userId} = req.params
+    const {id} = req.params
+    calc1.findByItemId(userId, id)
+    .then(calc => {
+        if (calc) {
+            res.status(200).json(calc)
+        } else {
+            res.status(404).json({message: "Could not retrieve specific calculator by user ID"})
         }
     })
     .catch(err => {
         res.status(500).json(err)
     })
-});
+})
 
 router.post('/', (req, res) => {
     const newCalc = req.body;
@@ -61,7 +78,6 @@ router.post('/', (req, res) => {
     if (!clothingTotal) {
         res.status(422).json({message: "Missing fields: clothingTotal"})
     }
-    console.log(newCalc);
     calc1.add(newCalc)
     .then(calc => {
         res.status(201).json(calc)
@@ -106,7 +122,6 @@ router.put('/:id', (req, res) => {
     if (!clothingTotal) {
         res.status(422).json({message: "Missing updated fields: clothingTotal"})
     }
-    console.log(updatedCalc);
     calc1.update(id, updatedCalc)
     .then(calc => {
         if (calc) {
@@ -120,13 +135,64 @@ router.put('/:id', (req, res) => {
     })
 })
 
-router.delete('/:id', (req, res) => {
+router.put('/:userId/:id', (req, res) => {
+    const {id} = req.params;
+    const updatedCalc = req.body;
+    const {userId} = req.body;
+    const {foodTotal} = req.body;
+    const {transportationTotal} = req.body;
+    const {healthCareTotal} = req.body;
+    const {debtTotal} = req.body;
+    const {housingTotal} = req.body;
+    const {utilitiesTotal} = req.body;
+    const {clothingTotal} = req.body;
+    if (!userId) {
+        res.status(422).json({message: "Missing updated fields: userId"})
+    }
+    if (!foodTotal) {
+        res.status(422).json({message: "Missing updated fields: foodTotal"})
+    }
+    if (!transportationTotal) {
+        res.status(422).json({message: "Missing updated fields: transportationTotal"})
+    }
+    if (!healthCareTotal) {
+        res.status(422).json({message: "Missing updated fields: healthCareTotal"})
+    }
+    if (!debtTotal) {
+        res.status(422).json({message: "Missing updated fields: debtTotal"})
+    }
+    if (!housingTotal) {
+        res.status(422).json({message: "Missing updated fields: housingTotal"})
+    }
+    if (!utilitiesTotal) {
+        res.status(422).json({message: "Missing updated fields: utilitiesTotal"})
+    }
+    if (!clothingTotal) {
+        res.status(422).json({message: "Missing updated fields: clothingTotal"})
+    }
+    calc1.updateByItemId(userId, id, updatedCalc)
+    .then(calc => {
+        if (calc) {
+        res.status(201).json(calc)
+        } else {
+        res.status(404).json({message: "This personal calculator for this user does not exist!"})
+        }
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json(err)
+    })
+})
+
+router.delete('/:userId/:id', (req, res) => {
+    const {userId} = req.params
     const {id} = req.params
-    calc1.remove(id)
+    calc1.remove(userId, id)
     .then(count => {
         if (count > 0) {
             res.status(200).json({message: "This calculator for this user is now removed"})
         } else {
+            console.log(err)
             res.status(404).json({message: "This calculator does not exist!"})
         }
     })
